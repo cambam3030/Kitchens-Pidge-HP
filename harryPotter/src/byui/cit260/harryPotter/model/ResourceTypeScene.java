9372d22577661b1403682ef1f185bbc9faf66b6c
Cami <Cami@Cami>
2017-01-27 21:00:27 -0600

    01/27/17 Created classes Location, Character, Scene, CharacterScene, ResourceTypeScene, HousePoints

---
+++ harryPotter/src/byui/cit260/harryPotter/model/ResourceTypeScene.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.harryPotter.model;
+
+import java.util.Objects;
+
+
+public class ResourceTypeScene extends Scene {
+    
+
+    public ResourceTypeScene() {
+    }
+        
+    //class instance variables
+    private String resourceType;
+    private double amountAvailable;
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public double getAmountAvailable() {
+        return amountAvailable;
+    }
+
+    public void setAmountAvailable(double amountAvailable) {
+        this.amountAvailable = amountAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "RescourseTypeScene{" + "resourceType=" + resourceType + ", amountAvailable=" + amountAvailable + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + Objects.hashCode(this.resourceType);
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.amountAvailable) ^ (Double.doubleToLongBits(this.amountAvailable) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ResourceTypeScene other = (ResourceTypeScene) obj;
+        if (Double.doubleToLongBits(this.amountAvailable) != Double.doubleToLongBits(other.amountAvailable)) {
+            return false;
+        }
+        if (!Objects.equals(this.resourceType, other.resourceType)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
