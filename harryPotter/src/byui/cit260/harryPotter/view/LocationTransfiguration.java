091c81ab9a8d8ecf1fe4a029a33f46ccb24007c8
Cami <Cami@Cami>
2017-04-01 22:16:26 -0500

    Added content to the Locations Dormitory, Charms, Herboloby, Library, Transfiguration, HagridsHut, ForbiddenForest, RoomOfRequirements, HeadMastersoffice

--- harryPotter/src/byui/cit260/harryPotter/view/LocationTransfiguration.java
+++ harryPotter/src/byui/cit260/harryPotter/view/LocationTransfiguration.java
@@ -12,7 +12,13 @@ package byui.cit260.harryPotter.view;
    public class LocationTransfiguration extends ViewMenu{
     
     public LocationTransfiguration(){
-        super("");
+        super("\n Welcome to Transfiguration Class. Here you will learn to tranform "
+                + "\n even the most mundane of objects. "
+                + "\n"
+                + "\n PC Turn a Hedgehog into a Pincushion"
+                + "\n M Turn a Match into a Needle"
+                + "\n T Turn a Teapot into a Tortoise"
+                + "\n R Return ");
     }
     
     @Override
@@ -22,7 +28,18 @@ package byui.cit260.harryPotter.view;
         boolean done = false;
         
         switch(choice) {
-            case "":
+            case "PC":
+                console.println("\n You paid close attention and completed the "
+                        + "\n transfiguration perfectly. You pins are even "
+                        + "\n color coded.");
+            case "M":
+                console.println("You were too busy talking to Ron. The shape of the "
+                        + "\n needle is right, but you failed to turn it to metal.");
+            case "T":
+                console.println("You achieved a tortoise, but steam is issuing from "
+                        + "\n its mouth and its making a faint whistling sound.");
+            case "R":
+                return true;
             
             default:
                 this.console.println("\n*** Invalid selection *** Try again ***");
